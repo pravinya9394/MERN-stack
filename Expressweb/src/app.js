9841efffe86fb6a9bfb64e13290c1c5d5d0035b9
Pravinya K <pravinya.k.in@ad.rupeek.net>
2023-02-07 19:30:00 +0530

    Added frontend part and views folder

--- Expressweb/src/app.js
+++ Expressweb/src/app.js
@@ -1,7 +1,12 @@
 const express=require("express");
+const path=require("path");
 const app=express();
 const port=process.env.PORT||8000;
 
+const static_path=path.join(__dirname,"../public");
+
+
+app.use(express.static(static_path));
 // HOME PAGE
 app.get("/",(req,res)=>{
     res.send("Welcome to Home page");
