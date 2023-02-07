15456d30710938a6649674d9c9db74634d461750
Pravinya K <pravinya.k.in@ad.rupeek.net>
2023-02-07 22:57:44 +0530

    Added new files

--- Expressweb/src/app.js
+++ Expressweb/src/app.js
@@ -1,20 +1,26 @@
 const express=require("express");
 const path=require("path");
 const app=express();
+const hbs=require("hbs");
 const port=process.env.PORT||8000;
 
 const static_path=path.join(__dirname,"../public");
-
+const template_path=path.join(__dirname,"../templates/views");
+const partial_path=path.join(__dirname,"../templates/partials")
+app.set("view engine","hbs");
+app.set("views",template_path);
+hbs.registerPartials(partial_path);
 
 app.use(express.static(static_path));
 // HOME PAGE
+
 app.get("/",(req,res)=>{
-    res.send("Welcome to Home page");
+    res.render("index");
 });
 
 // ABPUT PAGE
 app.get("/about",(req,res)=>{
-    res.send("Welcome to About page");
+    res.render("about");
 });
 
 // WEATHER PAGE
@@ -31,4 +37,3 @@ app.get("*",(req,res)=>{
 app.listen(8000,()=>{
     console.log(`Running on port ${port}`)
 });
-
